4d96bb06d735910d5e9cd546005bc7be73073e1a
unknown <vamshikrishna.vk67@gmail.com>
2024-06-21 02:09:50 +0530

    Updates

---
+++ precache-manifest.719b1e6b33d8d7455b3a2bd959cfffb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6683ce0a561f0b90d88c7ee684d1840d",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "b64d0a9c987dc7411d56",
+    "url": "/React/static/css/main.18db1a04.chunk.css"
+  },
+  {
+    "revision": "2cd7cb11aa90e67664bf",
+    "url": "/React/static/js/2.58d680d1.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/React/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "b64d0a9c987dc7411d56",
+    "url": "/React/static/js/main.25a7f2bf.chunk.js"
+  },
+  {
+    "revision": "550c68a451fdff68235e",
+    "url": "/React/static/js/runtime~main.fd810306.js"
+  }
+]);
